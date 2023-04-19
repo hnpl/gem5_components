1646bda7882e024833e439eb495ceda7a1d104bb
Hoa Nguyen <hoanguyen@ucdavis.edu>
2023-04-19 11:34:22 -0700

    Change how VectorParamters object is applied
    In gem5, ARM's SVE are configured at different places depending on
    the simulation mode.
    - In SE mode, the sve configuration is set in ISA object's sve_vl_se
    parameter.
    - In FS mode, the sve configuration is set in System object's sve_vl
    parameter.
    
    Signed-off-by: Hoa Nguyen <hoanguyen@ucdavis.edu>

--- vector_cores/vector_cores.py
+++ vector_cores/vector_cores.py
@@ -12,42 +12,59 @@ from gem5.components.processors.switchable_processor import SwitchableProcessor
 from gem5.components.processors.cpu_types import CPUTypes, get_mem_mode
 from gem5.isas import ISA
 
-from m5.objects import BaseISA
+from m5.objects import BaseISA, System
 from m5.util import warn
 
 class BaseVectorParameters:
-    def __init__(self):
-        pass
-    def apply(self, core: SimpleCore):
+    def __init__(self, is_fullsystem: bool):
+        self.is_fullsystem = is_fullsystem
+    def apply_core_change(self, core: SimpleCore):
         self._apply_isa_change(core.isa)
         self._apply_isa_change(core.decoder[0].isa)
+    def apply_system_change(self, system_object: System):
+        self._apply_system_change(system_object)
     @abstractmethod
     def _apply_isa_change(self, isa_object: BaseISA):
         pass
+    @abstractmethod
+    def _apply_system_change(self, system_object: System):
+        pass
 
 class ARM_SVE_Parameters(BaseVectorParameters):
-    def __init__(self, vlen):
+    def __init__(self, vlen, is_fullsystem: bool):
+        super().__init__(is_fullsystem)
         self.vlen = vlen
         # in gem5, the vector register length (vlen) is sve_vl * 128
-        requires((vlen % 128 == 0) and (vlen > 0) and (vlen <= 2048))
+        assert((vlen % 128 == 0) and (vlen > 0) and (vlen <= 2048))
     @overrides(BaseVectorParameters)
     def _apply_isa_change(self, isa_object: BaseISA):
-        isa_object.sve_vl = self.vlen // 128
+        if not self.is_fullsystem:
+            isa_object.sve_vl_se = self.vlen // 128
+    @overrides(BaseVectorParameters)
+    def _apply_system_change(self, system_object: System):
+        if self.is_fullsystem:
+            system_object.sve_vl = self.vlen // 128
 
 class RVV_Parameters(BaseVectorParameters):
-    def __init__(self, elen, vlen):
+    def __init__(self, elen, vlen, is_fullsystem: bool):
+        super().__init__(is_fullsystem)
         self.elen = elen
         self.vlen = vlen
     @overrides(BaseVectorParameters)
     def _apply_isa_change(self, isa_object: BaseISA):
         isa_object[0].elen = self.elen
         isa_object[0].vlen = self.vlen
+    @overrides(BaseVectorParameters)
+    def _apply_system_change(self, system_object: System):
+        raise NotImplementedError(
+            "RVV configuration does not affect system object"
+        )
 
 class SimpleVectorCore(SimpleCore):
     def __init__(self, cpu_type: CPUTypes, core_id: int, isa: ISA,
             isa_vector_parameters: BaseVectorParameters):
         super().__init__(cpu_type, core_id, isa)
-        isa_vector_parameters.apply(self.core)
+        isa_vector_parameters.apply_core_change(self.core)
 
 class SimpleVectorProcessor(BaseCPUProcessor):
     def __init__(self, cpu_type: CPUTypes, num_cores: int, isa: ISA,
