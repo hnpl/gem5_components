8db02884ad9c006a1c00956603c46a61140bdd2a
Hoa Nguyen <hoanguyen@ucdavis.edu>
2023-04-21 22:37:39 +0000

    Add params classes for permutating_gather and permutating_scatter
    Signed-off-by: Hoa Nguyen <hoanguyen@ucdavis.edu>

---
+++ workloads_params/permutating_scatter.py
@@ -0,0 +1,24 @@
+from .isa_extensions import ISAExtension
+from .params import Params
+
+from pathlib import Path
+from typing import List
+
+class GUPSParams(Params):
+    def __init__(self, source_path: Path, with_roi_annotations: bool, seed: int, mod: int, isa_extensions: List[ISAExtension] = []) -> None:
+        super().__init__(source_path, with_roi_annotations, isa_extensions)
+        self.seed = seed
+        self.mod = mod
+
+    def get_command(self) -> str:
+        binary_name = ["permutating_scatter", "hw"]
+        if self.isa_extensions:
+            binary_name.extend(self.isa_extensions)
+        if self.with_roi_annotations:
+            binary_name.append("m5")
+        binary_name = ".".join(binary_name)
+        path = self.source_path / binary_name
+        return f"{str(path)} {self.seed} {self.mod}"
+
+    def get_naming_string(self) -> str:
+        return "-".join(["permutating_scatter", str(self.seed), str(self.mod)])
